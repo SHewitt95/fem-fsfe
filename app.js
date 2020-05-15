5d0a18aae154338fc5297d641910a62ed77e9c63
Sherman Hewitt <hewitt.sherman@gmail.com>
2020-05-15 09:04:34 -0400

    add route that returns 418 status

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/yolo', (req, res) => {
+  res.status(418).send('I bet you thought I was real');
+});
+
 app.listen(port, () => console.log(`Here we are. Back again. ${port}`));
